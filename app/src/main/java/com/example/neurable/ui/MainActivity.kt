93a9bc40e59467fad93c838ddad711cef7d43b2a
Ben Tseytlin <btseytlin@thecommonsproject.org>
2024-12-03 15:21:00 -0500

    Completed Neurable Android Technical Challenge

--- app/src/main/java/com/example/neurable/ui/MainActivity.kt
+++ app/src/main/java/com/example/neurable/ui/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -19,7 +18,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.neurable.R
 import com.example.neurable.data.bluetooth.model.BluetoothState
 import com.example.neurable.data.focusscore.model.FocusScore
@@ -39,26 +37,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NeurableTheme {
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            colors = TopAppBarDefaults.topAppBarColors(
-                                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                                titleContentColor = MaterialTheme.colorScheme.primary,
-                            ),
-                            title = {
-                                Text("Neurable")
-                            }
+                Column {
+                    TopAppBar(
+                        title = { Text(text = "Adopt Me") },
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                            titleContentColor = MaterialTheme.colorScheme.primary,
                         )
-                    },
-                ) { innerPadding ->
-                    HomePage(modifier = Modifier.padding(innerPadding))
+                    )
+                    HomePage(modifier = Modifier)
                 }
             }
         }
     }
 
-    @Preview
     @Composable
     fun HomePage(
         modifier: Modifier = Modifier,
@@ -69,9 +61,12 @@ class MainActivity : AppCompatActivity() {
 
         val bluetoothStateValue = bluetoothState.value
         if (bluetoothStateValue is BluetoothState.AvailableDevices) {
-            ConnectBluetoothCard(bluetoothStateValue, { bluetoothDevice ->
-                mainActivityViewModel.connectDevice(bluetoothDevice)
-            })
+            ConnectBluetoothCard(
+                bluetoothStateValue, { bluetoothDevice ->
+                    mainActivityViewModel.connectDevice(bluetoothDevice)
+                },
+                modifier = modifier
+            )
         } else {
             Column(
                 modifier = modifier
